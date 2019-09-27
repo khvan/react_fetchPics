7edd12ec0b1e2970b6e05edb624ae8da246003ec
kBoris <khvanboris@gmail.com>
2019-09-27 12:56:11 -0700

    added css grid system and styling

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,5 +1,16 @@
 import React from 'react';
+import './ImageList.css';
+import ImageCard from './ImageCard';
 
-const ImageList = (props)=>{
-  <div>Image list my friend</div>
-}
+const ImageList = props => {
+  const images = props.images.map (image => {
+    return <ImageCard key={image.id} image={image} />;
+  });
+  return (
+    <div className="image-list">
+      {images}
+    </div>
+  );
+};
+
+export default ImageList;
